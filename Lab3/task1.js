3c5df8804690fb1d0ec422bdf1744c9c6268adc9
AmbidexterT <55133073+AmbidexterT@users.noreply.github.com>
2020-05-12 14:09:18 +0300

    Update task1.js

--- Lab3/task1.js
+++ Lab3/task1.js
@@ -1,6 +1,8 @@
 const ITMO_BORN_YEAR = 1990;
 const ITMO_BORN_MONTH = 3;
 const ITMO_BORN_DAY = 26;
-var str = ITMO_BORN_YEAR + " " + ITMO_BORN_MONTH + " " +ITMO_BORN_DAY;
-var itmoBornDate = new Date(str);
-console.log(itmoBornDate.toDateString());
+
+
+var itmoBornDate = new Date(ITMO_BORN_YEAR, ITMO_BORN_MONTH - 1, ITMO_BORN_DAY);
+
+console.log(itmoBornDate.toDateString()); // Mon Mar 26 1990
